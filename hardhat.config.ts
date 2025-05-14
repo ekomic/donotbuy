8c2f4b5a24877e82fc6ead9281e7c0b9dbd6ea1f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-05-14 15:20:54 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ const config: HardhatUserConfig = {
           evmVersion: 'london',
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 10000,
           },
         },
       },
